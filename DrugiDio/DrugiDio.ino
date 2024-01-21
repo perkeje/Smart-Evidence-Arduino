572c0cefd7fa1cc6a811237d34ad0879a4ba6480
jperic <juraj.peric@barrage.net>
2024-01-21 23:55:22 +0100

    Added most of the code

---
+++ DrugiDio/DrugiDio.ino
@@ -0,0 +1,211 @@
+#include <ArduinoWebsockets.h>
+#include <ESP8266WiFi.h>
+#include <SPI.h>
+#include "printf.h"
+#include "RF24.h"
+#include <Servo.h>
+
+enum { READY,
+       AUTHENTICATING,
+       EVALUATING,
+       WAITING_FOR_RFID,
+       ADDING_RFID } state = READY;
+
+using namespace websockets;
+
+WebsocketsClient attendanceSocket;
+WebsocketsClient rfidSocket;
+
+const char* ssid = "Brzzi";  // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "131000041103";
+
+#define CE_PIN 4
+#define CSN_PIN 5
+RF24 radio(CE_PIN, CSN_PIN);
+Servo servo;
+
+uint8_t address[][6] = { "1Node", "2Node" };
+
+bool radioNumber = 0;
+
+bool role = false;
+
+byte uuid[10];
+bool canAttend;
+byte name[32] = { NULL };
+char* message = "#ADD_USER";
+
+void setup() {
+  pinMode(15, OUTPUT);
+  servo.attach(15);
+  pinMode(0, OUTPUT);
+  pinMode(16, OUTPUT);
+  pinMode(2, OUTPUT);
+  digitalWrite(0, LOW);
+  digitalWrite(16, LOW);
+  digitalWrite(2, LOW);
+  Serial.begin(115200);
+  while (!Serial) {
+    // some boards need to wait to ensure access to serial over USB
+  }
+
+  // initialize the transceiver on the SPI bus
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}  // hold in infinite loop
+  }
+  // Set the PA Level low to try preventing power supply related problems
+  // because these examples are likely run with nodes in close proximity to
+  // each other.
+  radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
+
+  // save on transmission time by setting the radio to only transmit the
+  // number of bytes we need to transmit a float
+  radio.setPayloadSize(sizeof(uuid));  // float datatype occupies 4 bytes
+
+  // set the TX address of the RX node into the TX pipe
+  radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
+
+  // set the RX address of the TX node into a RX pipe
+  radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
+  radio.setChannel(150);
+
+  // additional setup specific to the node's role
+  if (role) {
+    radio.stopListening();  // put radio in TX mode
+  } else {
+    radio.startListening();  // put radio in RX mode
+  }
+
+  WiFi.begin(ssid, password);  // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) {  // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i);
+    Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+
+  // bool connected = false;
+  // while (!connected) {
+  //   Serial.println("WS connecting");
+  //   connected = attendanceSocket.connect("ws://raspberrypi.local:8888/evidence_ws");
+  //   delay(500);
+  // }
+  bool connected = false;
+  while (!connected) {
+    Serial.println("WS connecting");
+    connected = rfidSocket.connect("ws://raspberrypi.local:8888/rfid_ws");
+    delay(500);
+  }
+  rfidSocket.onMessage([&](WebsocketsMessage recievedMessage) {
+    if (strcmp(recievedMessage.c_str(), "#ADD_USER") == 0) {
+      radio.stopListening();
+      radio.setPayloadSize(strlen(message) + 1);
+      state = ADDING_RFID;
+    }
+  });
+}
+
+void loop() {
+
+
+  if (state == READY) {
+    if (rfidSocket.available()) {
+      rfidSocket.poll();
+    }
+    if (radio.available()) {
+
+      uint8_t bytes = radio.getPayloadSize();
+      radio.read(&uuid, bytes);
+      digitalWrite(0, HIGH);
+      radio.stopListening();
+      radio.setPayloadSize(sizeof(name));
+      int i = 0;
+      Serial.println("upisi ime");
+      while (Serial.available() == 0) {}
+      Serial.readBytesUntil('\n', name, 32);
+      state = AUTHENTICATING;
+    }
+    delay(200);
+  }
+
+  if (state == ADDING_RFID) {
+    bool report = radio.write(message, strlen(message) + 1);
+    if (report) {
+      radio.startListening();
+      radio.setPayloadSize(sizeof(uuid));
+      state = WAITING_FOR_RFID;
+    } else {
+      Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
+      delay(300);
+    }
+  }
+
+  if (state == WAITING_FOR_RFID) {
+    if (radio.available()) {
+      // is there a payload? get the pipe number that recieved it
+      uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
+      radio.read(&uuid, bytes);
+      String uuidStr = "";
+      for (int i = 0; i < bytes; i++) {
+        // Convert each byte to a hexadecimal string and append
+        if (uuid[i] < 0x10) uuidStr += "0";  // Leading zero for single digit hex
+        uuidStr += String(uuid[i], HEX);
+      }
+
+      // Send the string via WebSocket
+      rfidSocket.send(uuidStr.c_str());
+      radio.startListening();
+      state = READY;
+    }
+  }
+
+  if (state == AUTHENTICATING) {
+    bool report = radio.write(&name, sizeof(name));
+    if (report) {
+      Serial.print(F("Transmission successful! "));  // payload was delivered
+      Serial.print(F("Time to transmit = "));        // print the timer result
+      Serial.print(F(" us. Sent: "));
+      Serial.println(String((char*)name));  // print payload sent
+      radio.startListening();
+      radio.setPayloadSize(sizeof(canAttend));
+      memset(name, NULL, 32);
+      memset(uuid, NULL, 10);
+      state = EVALUATING;
+    } else {
+      Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
+      delay(300);
+    }
+  }
+
+  if (state == EVALUATING) {
+    if (radio.available()) {
+      // is there a payload? get the pipe number that recieved it
+      uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
+      radio.read(&canAttend, bytes);
+      digitalWrite(0, LOW);
+      if (canAttend) {
+        digitalWrite(2, HIGH);
+        servo.write(180);
+        delay(3000);
+        digitalWrite(2, LOW);
+        servo.write(-180);
+      } else {
+        digitalWrite(16, HIGH);
+        delay(3000);
+        digitalWrite(16, LOW);
+      }
+      radio.setPayloadSize(sizeof(uuid));
+      state = READY;
+    }
+  }
+}
---
+++ Projekt/Projekt.ino
@@ -0,0 +1,336 @@
+enum { READY,
+       SCANNED,
+       WAITING_FOR_AUTH,
+       ALC_PREPARE,
+       TEMP_PREPARE,
+       MEASURING_ALC,
+       MEASURING_TEMP,
+       EVALUATION,
+       SENDING_RFID,
+       SCANNING_RFID } state = READY;
+// Define LCD screen
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+// Define  RFID sensor
+#include <SPI.h>
+#include <MFRC522.h>
+#define RST_PIN 9
+#define SS_PIN 10
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+// Define pin of alcohol sensor
+#define MQ3pin 0
+
+// Define nRF
+#include "printf.h"
+#include "RF24.h"
+#define CE_PIN A0
+#define CSN_PIN 8
+RF24 radio(CE_PIN, CSN_PIN);
+
+uint8_t address[][6] = { "1Node", "2Node" };
+bool radioNumber = 1;
+
+bool role = true;
+
+// Variable init
+byte nuidPICC[4];
+float R0 = 2800;
+float maxBAC = 0;
+float maxTemp = 0;
+byte name[32];
+bool canAttend;
+
+char* add_user = "#ADD_USER";
+
+long startTime;
+
+void setup() {
+  pinMode(A4, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) {
+    // some boards need to wait to ensure access to serial over USB
+  }
+
+  while (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    delay(1000);
+    // hold in infinite loop
+  }
+
+  radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
+
+
+  // set the TX address of the RX node into the TX pipe
+  radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
+
+  // set the RX address of the TX node into a RX pipe
+  radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
+  radio.setChannel(150);
+  // additional setup specific to the node's role
+
+
+
+  radio.startListening();  // put radio in RX mode
+  radio.setPayloadSize(strlen(add_user) + 1);
+
+  lcd.begin(16, 2);
+  SPI.begin();         // Init SPI bus
+  mfrc522.PCD_Init();  // Init MFRC522
+  delay(10);           // Optional delay. Some board do need more time after init to be ready, see Readme
+  mfrc522.PCD_DumpVersionToSerial();
+  lcd.clear();
+  lcd.print("Skeniraj");
+  lcd.setCursor(0, 1);
+  lcd.print("karticu...");
+}
+
+void loop() {
+
+  if (state == READY) {
+    if (radio.available()) {
+      uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
+      radio.read(add_user, bytes);
+
+      lcd.clear();
+      lcd.print("Dodavanje");
+      lcd.setCursor(0, 1);
+      lcd.print("Korisnika");
+      state = SCANNING_RFID;
+
+      radio.stopListening();
+      radio.setPayloadSize(sizeof(mfrc522.uid.uidByte));
+    } else {
+      // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+      if (!mfrc522.PICC_IsNewCardPresent()) {
+        return;
+      }
+
+      // Select one of the cards
+      if (!mfrc522.PICC_ReadCardSerial()) {
+        return;
+      }
+      state = SCANNED;
+      radio.stopListening();
+      radio.setPayloadSize(sizeof(mfrc522.uid.uidByte));
+      tone(A4, 1500, 150);
+      lcd.clear();
+      lcd.print("Autentifikacija");
+    }
+  }
+
+  if(state ==SCANNING_RFID) {
+    if (!mfrc522.PICC_IsNewCardPresent()) {
+        return;
+      }
+
+      // Select one of the cards
+      if (!mfrc522.PICC_ReadCardSerial()) {
+        return;
+      }
+      tone(A4, 1500, 150);
+      state = SENDING_RFID;
+  }
+
+  if (state == SENDING_RFID) {
+    
+    unsigned long start_timer = micros();
+    bool report = radio.write(&mfrc522.uid.uidByte, sizeof(mfrc522.uid.uidByte));
+    unsigned long end_timer = micros();
+
+    if (report) {
+      Serial.print(F("Transmission successful! "));
+      Serial.print(F("Time to transmit = "));
+      Serial.print(end_timer - start_timer);
+      Serial.print(F(" us. Sent: "));
+      lcd.clear();
+      lcd.print("Skeniraj");
+      lcd.setCursor(0, 1);
+      lcd.print("karticu...");
+      mfrc522.PICC_HaltA();
+      mfrc522.PCD_StopCrypto1();
+      state = READY;
+      radio.startListening();
+      radio.setPayloadSize(strlen(add_user) + 1);
+    }
+  }
+
+  if (state == SCANNED) {
+
+    unsigned long start_timer = micros();                                          // start the timer
+    bool report = radio.write(&mfrc522.uid.uidByte, sizeof(mfrc522.uid.uidByte));  // transmit & save the report
+    unsigned long end_timer = micros();                                            // end the timer
+
+    if (report) {
+      Serial.print(F("Transmission successful! "));  // payload was delivered
+      Serial.print(F("Time to transmit = "));
+      Serial.print(end_timer - start_timer);  // print the timer result
+      Serial.print(F(" us. Sent: "));
+
+      state = WAITING_FOR_AUTH;
+
+      radio.startListening();
+      radio.setPayloadSize(sizeof(name));
+    } else {
+      Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
+      delay(300);
+    }
+  }
+
+  if (state == WAITING_FOR_AUTH) {
+
+    if (radio.available()) {
+      uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
+      radio.read(&name, bytes);
+      lcd.clear();
+      String name_string = (char*)name;
+      int spaceIndex = name_string.indexOf(" ");
+      if (spaceIndex != -1) {
+        lcd.print(name_string.substring(0, spaceIndex));
+        lcd.setCursor(0, 1);
+      }
+      lcd.print(name_string.substring(spaceIndex + 1, name_string.length()));
+      delay(3000);
+      state = ALC_PREPARE;
+      lcd.clear();
+      lcd.print("Mjerenje");
+      lcd.setCursor(0, 1);
+      lcd.print("Alc. za ");
+      lcd.setCursor(14, 1);
+      lcd.print("3s");
+      startTime = millis();
+    }
+  }
+
+  if (state == ALC_PREPARE) {
+    float time = millis();
+    if (time - startTime < 3000) {
+      lcd.setCursor(14, 1);
+      lcd.print(3 - (int)((time - startTime) / 1000));
+    } else {
+      lcd.clear();
+      lcd.print("Pusi u senzor");
+      lcd.setCursor(14, 1);
+      lcd.print("5s");
+      state = MEASURING_ALC;
+      startTime = millis();
+    }
+  }
+
+  if (state == MEASURING_ALC) {
+    if (millis() - startTime < 5000) {
+      int remaining = (millis() - startTime) / 1000;
+      lcd.setCursor(14, 1);
+      lcd.print(5 - remaining);
+      lcd.print("s");
+      int sensorValue = analogRead(A5);
+      float sensorVolt = (float)sensorValue / 1024 * 5.0;
+      float RS_gas = ((5.0 * 2000) / sensorVolt) - 2000;
+      float ratio = RS_gas / R0;
+      double x = 0.4 * ratio;
+      float BAC = pow(x, -1.431);
+      if (BAC > maxBAC) {
+        maxBAC = BAC;
+      }
+      delay(100);
+    } else {
+      lcd.clear();
+      lcd.print(maxBAC);
+      lcd.print(" mg/L");
+      state = TEMP_PREPARE;
+      delay(3000);
+      lcd.clear();
+      lcd.print("Mjerenje");
+      lcd.setCursor(0, 1);
+      lcd.print("Temp. za ");
+      lcd.setCursor(14, 1);
+      lcd.print("3s");
+      startTime = millis();
+    }
+  }
+  if (state == TEMP_PREPARE) {
+    float time = millis();
+    if (time - startTime < 3000) {
+      lcd.setCursor(14, 1);
+      lcd.print(3 - (int)((time - startTime) / 1000));
+    } else {
+      lcd.clear();
+      lcd.print("Mjerenje Temp.");
+      lcd.setCursor(14, 1);
+      lcd.print("5s");
+      state = MEASURING_TEMP;
+      startTime = millis();
+    }
+  }
+
+  if (state == MEASURING_TEMP) {
+    if (millis() - startTime < 5000) {
+      int remaining = (millis() - startTime) / 1000;
+      lcd.setCursor(14, 1);
+      lcd.print(5 - remaining);
+      lcd.print("s");
+      // here will go code for temp measuring now it will only return 36.6
+      float temp = 36.6;
+      if (temp > maxTemp) {
+        maxTemp = temp;
+      }
+      delay(100);
+    } else {
+      lcd.clear();
+      lcd.print(maxTemp);
+      lcd.print(" C");
+      state = EVALUATION;
+      delay(3000);
+      if (maxBAC > 0.5 && maxTemp > 0.5) {
+        canAttend = false;
+      } else {
+        canAttend = true;
+      }
+      radio.stopListening();
+      radio.setPayloadSize(sizeof(canAttend));
+    }
+  }
+
+  if (state == EVALUATION) {
+
+    unsigned long start_timer = micros();
+    bool report = radio.write(&canAttend, sizeof(canAttend));
+    unsigned long end_timer = micros();
+
+    if (report) {
+      Serial.print(F("Transmission successful! "));
+      Serial.print(F("Time to transmit = "));
+      Serial.print(end_timer - start_timer);
+      Serial.print(F(" us. Sent: "));
+      lcd.clear();
+      if (canAttend) {
+        lcd.print("Ulaz dozvoljen");
+        tone(A4, 1500, 100);
+        delay(200);
+        tone(A4, 1500, 100);
+        delay(2800);
+      } else {
+        lcd.print("Ulaz zabranjen");
+        tone(A4, 1500, 1000);
+        delay(3000);
+      }
+
+      maxTemp = 0;
+      maxBAC = 0;
+      radio.setPayloadSize(strlen(add_user) + 1);
+      radio.startListening();
+      mfrc522.PICC_HaltA();
+      mfrc522.PCD_StopCrypto1();
+      lcd.clear();
+      lcd.print("Skeniraj");
+      lcd.setCursor(0, 1);
+      lcd.print("karticu...");
+      state = READY;
+      memset(name, NULL, 32);
+    } else {
+      Serial.println(F("Transmission failed or timed out"));
+      delay(300);
+    }
+  }
+}
